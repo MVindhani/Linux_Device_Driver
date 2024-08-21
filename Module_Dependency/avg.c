8c769a109b0bbf2d2e8a09d12ae352f2c635a313
Malik Vindhani <105340177+MVindhani@users.noreply.github.com>
2024-08-21 12:06:33 +0200

    Inputs for number is provided by user by command line

--- Module_Dependency/avg.c
+++ Module_Dependency/avg.c
@@ -1,17 +1,23 @@
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/moduleparam.h>
 #include "add.h"
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("This is moudle to calculate avergae");
 MODULE_AUTHOR("Malik Vindhani");
 
+static int x =0;
+static int y = 0;
+
+module_param(x,int,0644);
+module_param(y,int,0644);
 static int avg_init(void)
 {
 	printk("File : %s Function : %s Line : %d\n",__FILE__,__func__,__LINE__);
 	printk("Average fun : Init \n");
-	printk("Averge is %d\n",add(10,20)/2); /* add() function is defined in add.c file */
+	printk("Averge is %d\n",add(x,y)/2); /* add() function is defined in add.c file */
 	return 0;
 }
 
