5abfb33599410332a9da846ce35cc8e3dcd3ab28
Malik Vindhani <105340177+MVindhani@users.noreply.github.com>
2024-08-21 22:58:18 +0200

    Add files via upload

--- Dynamic_Char_Device/App.c
+++ Dynamic_Char_Device/App.c
@@ -8,14 +8,17 @@ int main()
     int fd;
 
     char user_buf[] = "Hello from USer Buffer";
+    char user_read_buf[100];
 
-    fd = open("/dev/DynamicCharDriver",O_RDWR,0777);
+    fd = open("/dev/Dynamic",O_RDWR,0777);
     if(fd < 0)
     {
         perror("Open fails..!!\n");
     }
     
     write(fd,user_buf,sizeof(user_buf));
+	
+    read(fd,user_read_buf,sizeof(user_read_buf));
     
     close(fd);
     
